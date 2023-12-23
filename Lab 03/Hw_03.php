9f2df0bb4a424cf232e8e9474dbe373fbf43d5af
PekoyamaPeko02 <65160324@go.buu.ac.th>
2023-12-24 02:54:08 +0700

    Update Hw_03.php

--- Lab 03/Hw_03.php
+++ Lab 03/Hw_03.php
@@ -2,31 +2,42 @@
 
 <head>
     <meta charset="utf-8">
-</head>
+    <link rel="stylesheet" href="style.css" />
+    </head>
 
 <body>
+    <style>
+        body{
+        background-color: rgb(174, 214, 241 );
+
+      }
+    </style>
     <center>
     <?php $multi_x = 2; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
     </center>
-    
+    <div class ="scroll-bg">
+        <div class = "scroll-div">
+            <center>
     <table>
         <tbody>
             <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
+                <td>
+                <?php 
+                for($i = 1 ; $i<25 ; $i++){
+                    echo "{$multi_x} x {$i} = ".($multi_x*$i)."<br/>"; 
+                    }
+                    ?>
+                </td>
             </tr>
         </tbody>
     </table>
-    <style>
-        table {
-            width: 150px;
-            height: 265px;
-        }
-    </style>
+    </center>
+    
+    </div>
+        
+    
+    
 </body>
 
 </html>
