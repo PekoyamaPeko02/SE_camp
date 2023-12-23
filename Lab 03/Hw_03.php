c9522a8a36a00d808702c0a2a545d077bccf94ba
PekoyamaPeko02 <65160324@go.buu.ac.th>
2023-12-24 00:15:17 +0700

    Create Hw_03.php

---
+++ Lab 03/Hw_03.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <center>
+    <?php $multi_x = 2; ?>
+    <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    </center>
+    
+    <table>
+        <tbody>
+            <tr>
+                <td></td>
+                <td>x</td>
+                <td></td>
+                <td>=</td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+    <style>
+        table {
+            width: 150px;
+            height: 265px;
+        }
+    </style>
+</body>
+
+</html>
