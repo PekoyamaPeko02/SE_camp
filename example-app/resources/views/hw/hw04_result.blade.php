90d302753f88ac6e41673ddedaa7a33ed65ac07e
PekoyamaPeko02 <65160324@go.buu.ac.th>
2024-02-08 22:58:55 +0700

    new

---
+++ example-app/resources/views/hw/hw04_result.blade.php
@@ -0,0 +1,63 @@
+<html>
+
+    <head>
+        <meta charset="utf-8"/>
+        {{-- <link rel ="stylesheet" href="{{ asset('css/myhwstyle.css') }}"/> --}}
+</head>
+<body>
+    <style>
+        body{
+            background-image: url(https://img.freepik.com/free-photo/abstract-pastel-grid-background_53876-104004.jpg?w=1380&t=st=1704823104~exp=1704823704~hmac=91cca6085ab1ccea2bd6666fe9854e1368d5797c54c770285bb7fe625922df48);
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size:cover;
+        }
+        h1{
+            margin: 0;
+            padding: 0;
+            text-align: center;
+            position: absolute;
+            top:10%;
+            left: 50%;
+            transform: translatex(-50%);
+            font-size: 50px;
+            color: #705348;
+        }
+        .font{
+            margin: 0;
+            padding: 0;
+            text-align: center;
+
+            position: absolute;
+            top:20%;
+            left: 50%;
+            transform: translatex(-65%);
+            font-size: 20px;
+            width: 300px;
+
+        }
+        .bg{
+            background: rgb(255,202,212);
+            width: 400px;
+            height: 600px;
+            margin: 0 auto;
+            border: 5px solid gray;
+        }
+
+        </style>
+        <h1>Multiplication Table</h1>
+
+        <div class="font">
+            <div class="bg" >
+
+        <?php
+        for($i=1 ; $i<25 ; $i++){
+            echo "{$mynumber} x {$i} =".($mynumber*$i)."<br/>";
+
+        }
+        ?>
+        </div>
+
+</body>
+
+</html>
---
+++ example-app/routes/web.php
@@ -0,0 +1,89 @@
+<?php
+
+use App\Http\Controllers\MyController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Http;
+use Illuminate\Http\Request;
+use App\Http\Controllers\C_titles;
+use App\Http\Controllers\MyAuth;
+// use App\Models\M_titles;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/login', [MyAuth::class, 'login_view'])->name('login');
+Route::get('/register', [MyAuth::class, 'register_view']);
+Route::get('/logout', [MyAuth::class, 'logout_process']);
+Route::post('/login', [MyAuth::class, 'login_process']);
+Route::post('/register', [MyAuth::class, 'register_process']);
+
+Route::resource('titles', C_titles::class)->middleware('auth');
+Route::middleware('auth')->group(function(){
+    // auth first
+});
+
+
+
+
+Route::get('/my-controller', [MyController::class,'index']);
+Route::get('/my-controller2', 'App\Http\Controllers\MyController@index');
+
+Route::namespace('App\Http\Controllers\MyController')->group(function(){
+    Route::get('/my-controller3','MyController@index');
+
+});
+Route::resource('/my-controller4', MyController::class);
+
+Route::get('/', function () {
+    return view('welcome'); //full name = welcome.blade.php
+});
+
+
+// use Illumimatr\Http\Request
+Route::get('/my-route', function(){
+    // return view('myroute');
+    // Key => Value
+
+    $data  ['val_b'] = "Laravel";
+    return view('myfolder.mypage',$data);
+});
+
+
+
+// Route::post('/my-route', function(Request $req ){
+
+//     $data['myinput'] = $req ->input('myinput');
+//     return view('myroute',$data);
+
+// });
+
+
+// // // hw#4
+// Route::get('/home-work', function(){
+//     return view('hw.hw04');
+// });
+
+// Route::post('/home-work',function(Request $req){
+//     $data['mynumber'] = $req ->input('mynumber');
+//     return view('hw.hw04_result',$data);
+// });
+
+
+//15/01/2024
+
+Route::resource('titles', C_titles::class);
+
+// //hw#5
+// Route:: get('/home-work05',function(){
+//     return view('hw.hw05');
+// });
+
+
